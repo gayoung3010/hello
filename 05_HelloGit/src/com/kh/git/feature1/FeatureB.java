3ffe28096e7fce536b71fcef34d2f0bec1682a7f
gayoung3010 <zzzzc_c@naver.com>
2020-09-02 23:45:31 +0900

    add class FeatureB!

---
+++ 05_HelloGit/src/com/kh/git/feature1/FeatureB.java
@@ -0,0 +1,14 @@
+package com.kh.git.feature1;
+
+public class FeatureB {
+
+	public void maste() {
+		System.out.println("개발자의 마스터가 되기위한 과정");
+	}
+	
+
+	public void feat() {
+		System.out.println("진정한 목표를 달성하기 위해 내가 이루어야 하는 조건들");
+	}
+
+}
